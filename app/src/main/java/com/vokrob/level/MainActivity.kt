bca27ab8d8e8499027c6691e6a64c1a2809bb955
vokrob <106639488+vokrob@users.noreply.github.com>
2024-11-18 20:32:32 +0700

    Use of Magnetic Field and Accelerometer

--- app/src/main/java/com/vokrob/level/MainActivity.kt
+++ app/src/main/java/com/vokrob/level/MainActivity.kt
@@ -1,36 +1,71 @@
 package com.vokrob.level
 
 import android.content.Context
+import android.graphics.Color
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
 import android.os.Bundle
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     lateinit var sManager: SensorManager
+    private var magnetic = FloatArray(9)
+    private var gravity = FloatArray(9)
+
+    private var accrs = FloatArray(3)
+    private var magf = FloatArray(3)
+    private var values = FloatArray(3)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val tvSensor = findViewById<TextView>(R.id.tvSensor)
+        val lRotation = findViewById<LinearLayout>(R.id.lRotation)
         sManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
         val sensor = sManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+        val sensor2 = sManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD)
         val sListener = object : SensorEventListener {
 
-            override fun onSensorChanged(sEvent: SensorEvent?) {
-                val value = sEvent?.values
-                val sData = "X: ${value?.get(0)} \n Y: ${value?.get(1)} \n Z: ${value?.get(2)}"
-                tvSensor.text = sData
+            override fun onSensorChanged(event: SensorEvent?) {
+                when (event?.sensor?.type) {
+                    Sensor.TYPE_ACCELEROMETER -> accrs = event.values.clone()
+                    Sensor.TYPE_MAGNETIC_FIELD -> magf = event.values.clone()
+                }
+                SensorManager.getRotationMatrix(gravity, magnetic, accrs, magf)
+
+                val outGravity = FloatArray(9)
+                SensorManager.remapCoordinateSystem(
+                    gravity,
+                    SensorManager.AXIS_X,
+                    SensorManager.AXIS_Z,
+                    outGravity
+                )
+                SensorManager.getOrientation(outGravity, values)
+
+                val degree = values[2] * 57.295779513f
+                val rotate = 270 + degree
+                lRotation.rotation = rotate
+
+                val rData = 90 + degree
+                val color = if (rData.toInt() == 0) {
+                    Color.GREEN
+                } else {
+                    Color.RED
+                }
+                lRotation.setBackgroundColor(color)
+                tvSensor.text = rData.toInt().toString()
             }
 
             override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
             }
         }
         sManager.registerListener(sListener, sensor, SensorManager.SENSOR_DELAY_NORMAL)
+        sManager.registerListener(sListener, sensor2, SensorManager.SENSOR_DELAY_NORMAL)
     }
 }
 
